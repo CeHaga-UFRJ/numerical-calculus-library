10e73bfd07bdfb08cca99180e069b95833d340d0
Carlos Bravo <carlosh.serrado@gmail.com>
2022-02-26 17:55:09 -0300

    Draft files

---
+++ NumericalCalculus.jl
@@ -0,0 +1,14 @@
+#=
+  Implementacao de varios metodos de calcnum ... agora estou enrolando falando da nossa biblioteca
+=#
+
+#=
+Essa funcao eh uma funcao de teste e serve para mostrar como montar uma funcao
+
+x: O que seu coracao mandar
+
+Retorno: A funcao sempre vai retornar 42
+=#
+function teste(x)
+    return 42
+end 
