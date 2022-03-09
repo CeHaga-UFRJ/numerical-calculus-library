3dfec04bb2b10057c588dc81484ed7104b148493
Gabrielly de Andrade <gabrielly.andrade@dp6.com.br>
2022-03-09 20:10:06 -0300

    feat: adiciona função para problema 4

--- NumericalCalculus.jl
+++ NumericalCalculus.jl
@@ -17,197 +17,6 @@ end
 
 
 
-#============================== Funções de 2 a 8 ===========================#
-
-## 2. Problema: Achar um valor aproximado de uma função com informações de derivadas
-
-@doc raw"""
-Objetivo
-------------------------------
-Achar um valor aproximado de uma função com informações de derivadas utilizando o método de Taylor
-
-Especificação
-------------------------------
-|y - f(x)|<= E
-
-
-Parâmetros de entrada
-------------------------------
-    x : Number
-        Valor aproximado que queremos calcular a aproximação em y (f(x))
-
-    a : Number
-        Ponto fixo que será calculado a aproximação
-        Idealmente é próximo de x e é conhecida as derivadas no ponto
-
-    derivatives : Vector{Number}
-        Vetor com informações das derivadas no ponto a: f(a), f'(a), f''(a) ...
-
-    M: Number
-        Maior valor da n-derivada no intervalo (teto)
-
-    n: Int64, optional
-        Número de termos do polinômio de Taylor
-        Se nenhum valor for passado será calculado o polinômio de ordem 2
-
-
-Retorno
-------------------------------
-    y : Flotat64
-        Retorna o valor aproximado de f(x)
-
-    E : Float64
-        Retorna o erro da aproximação
-
-Exceções
-------------------------------
-
-    AssertionError
-        Caso a quantidade de derivadas no vetor seja insuficiente para realizar o 
-        cálculo, levanta exeção de domínio
-
-"""
-function value_approximation(x::Number, a::Number, derivatives::Vector{Number}, M::Number, n::Int64=2)
-    factor = 1
-    error = (x-a)^factor/factor
-    
-    @assert(size(derivatives)[1] > n, "Informe um conjunto de derivadas que seja maior que o valor n (ordem do polinômio)")
-    
-    sum = derivatives[1]
-    
-    for factor=factor:n
-        sum += derivatives[factor+1] * (((x-a)^factor)/factorial(factor))
-        error = ((x-a)^(factor+1))/factorial(factor+1)
-    end
-    
-    return sum, error
-    
-end
-
-## 3. Problema: Encontrar raíz aproximadamente (Resolver equações não-lineares) (uma variável e uma equação)
-
-
-@doc raw"""
-Objetivo
-------------------------------
-Encontrar zero de função (raiz) para calcular aproximação de valores numéricos com o método da Bisseção.
-Retorna um aviso caso o intervalo passado pelo usuário não possua sinais trocados.
-
-Especificação
-------------------------------
-
-f(r)=0 e |x-r| <= erro
-
-
-Parâmetros
-------------------------------
-    f : Function
-        Recebe uma função 
-
-    a : Number
-        Início do intervalo
-        Obrigatório caso o método seja Bisseção
-
-    b : Number
-        Fim do intervalo
-        Obrigatório caso o método seja Bisseção
-
-    error: Number
-        Erro no domínio
-        Obrigatório caso o método seja Bisseção
-
-    method: String, optional
-        Método que será aplicado a aproximação da raiz da função (:bisecion ou :newton)
-        Caso nenhum método seja escolhido o padrão é o da Bisseção
-
-
-Retorno
-------------------------------
-    root : Float64
-        Retorna uma aproximação para a raiz calculada pelo método da Bisseção
-
-Exceções
-------------------------------
-    AssertionError
-        Caso o intervalo passado não tenha troca de valores no intervalo (sinais opostos) para o método da Bisseção
-
-"""
-function find_root(f::Function, a::Number, b::Number, error::Number) 
-    
-    @assert(f(a)*f(b) < 0, "Aviso!! A função no intervalo passado [$a, $b] não possui sinais opostos")
-    
-    n = floor(log2((b - a) / error)) + 1
-    
-    for i=1:n
-        average = (b + a)/2
-        
-        if f(a) * f(average) < 0 # Verifica se o valor da função nos pontos são opostos
-            b = average # Estreitando o resultado pelo lado esquerdo
-        else
-            a = average # Estreitando o resultado pelo lado direito
-        end
-    end
-    
-    aprox = (b + a)/2
-    return aprox
-
-end
-
-
-@doc raw"""
-Objetivo
-------------------------------
-Encontrar zero de função (raiz) para calcular aproximação de valores numéricos com o método de Newton
-
-Especificação
-------------------------------
-
-f(r)=0
-
-Parâmetros
-------------------------------
-    f : Function
-        Recebe uma função 
-
-    derivative: Function
-        Derivada da função f
-
-    qtty_iterations: Int64, optional
-        Quantidade de interações para ser utilizada no método 
-        Caso nenhum valor seja passado será calculado 10 iterações
-
-    kick: Float64
-        Chute da função inicial para começar a aplicar o método
-
-    method: String, optional
-        Método que será aplicado a aproximação da raiz da função (:bisecion ou :newton)
-        Caso nenhum método seja escolhido o padrão é o da Bisseção
-
-Retorno
-------------------------------
-    root : Float64
-        Retorna uma aproximação para a raiz calculada pelo método de Newton
-
-
-"""
-function find_root(f::Function, derivative::Function, kick::Number, qtty_iterations::Int64=10)
-    
-    for i=1:qtty_iterations
-       
-        kick = kick - f(kick) / derivative(kick) # Método de newton utilizando a equação da reta
-    end
-    
-    return kick
-
-end
-
-
-
-#============================== Fim Funções de 2 a 8 ===========================#
-
-
-
-
 function erro(A, x ,b)
     return norm(A * x - b)
 end 
@@ -413,6 +222,282 @@ function lu_decomposition(A)
     end
     return L, U
 end
+
+#============================== Funções dos problemas de 2 a 8 ===========================#
+
+## 2. Problema: Achar um valor aproximado de uma função com informações de derivadas
+
+@doc raw"""
+Objetivo
+------------------------------
+Achar um valor aproximado de uma função com informações de derivadas utilizando o método de Taylor
+
+Especificação
+------------------------------
+|y - f(x)|<= E
+
+
+Parâmetros de entrada
+------------------------------
+    x : Number
+        Valor aproximado que queremos calcular a aproximação em y (f(x))
+
+    a : Number
+        Ponto fixo que será calculado a aproximação
+        Idealmente é próximo de x e é conhecida as derivadas no ponto
+
+    derivatives : Vector{Number}
+        Vetor com informações das derivadas no ponto a: f(a), f'(a), f''(a) ...
+
+    M: Number
+        Maior valor da n-derivada no intervalo (teto)
+
+    n: Int64, optional
+        Número de termos do polinômio de Taylor
+        Se nenhum valor for passado será calculado o polinômio de ordem 2
+
+
+Retorno
+------------------------------
+    y : Flotat64
+        Retorna o valor aproximado de f(x)
+
+    E : Float64
+        Retorna o erro da aproximação
+
+Exceções
+------------------------------
+
+    AssertionError
+        Caso a quantidade de derivadas no vetor seja insuficiente para realizar o 
+        cálculo, levanta exeção de domínio
+
+"""
+function value_approximation(x::Number, a::Number, derivatives::Vector, M::Number, n::Int64=2)
+    factor = 1
+    error = (x-a)^factor/factor
+    
+    @assert(size(derivatives)[1] > n, "Informe um conjunto de derivadas que seja maior que o valor n (ordem do polinômio)")
+    
+    sum = derivatives[1]
+    
+    for factor=factor:n
+        sum += derivatives[factor+1] * (((x-a)^factor)/factorial(factor))
+        error = ((x-a)^(factor+1))/factorial(factor+1)
+    end
+    
+    return sum, error
+    
+end
+
+## 3. Problema: Encontrar raíz aproximadamente (Resolver equações não-lineares) (uma variável e uma equação)
+
+
+@doc raw"""
+Objetivo
+------------------------------
+Encontrar zero de função (raiz) para calcular aproximação de valores numéricos com o método da Bisseção.
+Retorna um aviso caso o intervalo passado pelo usuário não possua sinais trocados.
+
+Especificação
+------------------------------
+
+f(r)=0 e |x-r| <= erro
+
+
+Parâmetros
+------------------------------
+    f : Function
+        Recebe uma função 
+
+    a : Number
+        Início do intervalo
+        Obrigatório caso o método seja Bisseção
+
+    b : Number
+        Fim do intervalo
+        Obrigatório caso o método seja Bisseção
+
+    error: Number
+        Erro no domínio
+        Obrigatório caso o método seja Bisseção
+
+    method: String, optional
+        Método que será aplicado a aproximação da raiz da função (:bisecion ou :newton)
+        Caso nenhum método seja escolhido o padrão é o da Bisseção
+
+
+Retorno
+------------------------------
+    root : Float64
+        Retorna uma aproximação para a raiz calculada pelo método da Bisseção
+
+Exceções
+------------------------------
+    AssertionError
+        Caso o intervalo passado não tenha troca de valores no intervalo (sinais opostos) para o método da Bisseção
+
+"""
+function find_root(f::Function, a::Number, b::Number, error::Number) 
+    
+    @assert(f(a)*f(b) < 0, "Aviso!! A função no intervalo passado [$a, $b] não possui sinais opostos")
+    
+    n = floor(log2((b - a) / error)) + 1
+    
+    for i=1:n
+        average = (b + a)/2
+        
+        if f(a) * f(average) < 0 # Verifica se o valor da função nos pontos são opostos
+            b = average # Estreitando o resultado pelo lado esquerdo
+        else
+            a = average # Estreitando o resultado pelo lado direito
+        end
+    end
+    
+    aprox = (b + a)/2
+    return aprox
+
+end
+
+
+@doc raw"""
+Objetivo
+------------------------------
+Encontrar zero de função (raiz) para calcular aproximação de valores numéricos com o método de Newton
+
+Especificação
+------------------------------
+
+f(r)=0
+
+Parâmetros
+------------------------------
+    f : Function
+        Recebe uma função 
+
+    derivative: Function
+        Derivada da função f
+
+    qtty_iterations: Int64, optional
+        Quantidade de interações para ser utilizada no método 
+        Caso nenhum valor seja passado será calculado 10 iterações
+
+    kick: Float64
+        Chute da função inicial para começar a aplicar o método
+
+    method: String, optional
+        Método que será aplicado a aproximação da raiz da função (:bisecion ou :newton)
+        Caso nenhum método seja escolhido o padrão é o da Bisseção
+
+Retorno
+------------------------------
+    root : Float64
+        Retorna uma aproximação para a raiz calculada pelo método de Newton
+
+
+"""
+function find_root(f::Function, derivative::Function, kick::Number, qtty_iterations::Int64=10)
+    
+    for i=1:qtty_iterations
+       
+        kick = kick - f(kick) / derivative(kick) # Método de newton utilizando a equação da reta
+    end
+    
+    return kick
+
+end
+
+## 4. Problema: Interpolação Polinomial
+
+@doc raw"""
+Objetivo
+----------
+Monta uma matriz de Vandermonde de dado grau
+
+Parâmetros
+----------
+    x: Vector{Number}
+        Vetor usado como base
+    qtd_rows: Number
+        Quantidade de pontos
+    degree: Number
+        Grau do polinomio
+
+Retorno
+----------
+    Retorno: Matriz de Vandermonde
+"""
+function vandermonde(x::Vector, qtd_rows::Number, degree::Number) 
+    # Cria uma matriz vazia 
+    V = zeros(qtd_rows, degree+1)
+    
+    # Para cada coluna adiciona uma potencia de u
+    for i = 1:degree+1
+         V[:, i] = x.^(i-1)
+    end
+    
+    return V
+end
+
+function vandermonde_interpolation(x::Vector, y::Vector, degree::Number)
+
+    qtd_rows = length(y)
+    
+    V = vandermonde(x, qtd_rows, degree)
+
+    c = V \ y #resolve_sistema(V, y) FIXME: quando utilizo o resolve_sistema meu exemplo 2 não funciona
+    
+    f(x) = sum(c[n+1]*x^n for n in 0:degree)
+    
+    return f
+end
+
+@doc raw"""
+Objetivo
+----------
+Transforma um conjunto de pontos discretos em uma função contínua.
+
+Parâmetros
+----------
+    points : Vector{Tuple{Number, Number}}
+        Vetor com coordenadas (x,y)
+
+    degree : Number, optional
+        Grau da interpolação
+
+    method: Symbol, optional
+        Nome do método utilizado para a interpolação
+
+Retorno
+----------
+    function : function
+        Retorna uma função
+
+"""
+function interpolation(points::Vector, degree::Number=0, method::Symbol=:vandermond)
+    
+    if degree == 0
+        degree, = size(points)
+    end
+    
+    x = zeros(0)
+    y = zeros(0)
+    
+    for point in points
+        push!(x, point[1])
+        push!(y, point[2])
+    end
+    
+        
+    if method == :vandermond
+        return vandermonde_interpolation(x, y, degree)
+    end
+
+end
+
+
+
+#============================== Fim Funções de 2 a 8 ===========================#
 
 #=
 
