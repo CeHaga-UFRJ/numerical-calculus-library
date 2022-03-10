4d6d2770937b76306ea65bb496cdbcc9c5f66080
Markson Arguello <marksonarguello@gmail.com>
2022-03-10 12:34:46 -0300

    Funções 9-15

--- NumericalCalculus.jl
+++ NumericalCalculus.jl
@@ -17,212 +17,6 @@ end
 
 
 
-function erro(A, x ,b)
-    return norm(A * x - b)
-end 
-
-#=
-Função que calcula a norma de um vetor v.
-
-Dado um vetor v, a sua norma será a raiz quadrada do somatório dos seus elementos elevados ao quadrado
-
-Entrada: Um vetor v
-Saída: A norma do vetor v
-
-=#
-function norma(v)
-    v = v.^2 # Elevo os elementos ao quadrado
-    soma = sum(v) # Faço o somatório dos elementos ao quadrado
-    z = sqrt(soma) # Faço a raiz quadrada dessa soma    
-    return z
-end 
- 
-
-#=
-Função que resolve um sistema linear denso aproximadamente
-
-Dado uma matriz densa nxn A e um vetor b resolve o sistema A’Ax=A’b onde Ax* aproximadamente b ( x*=argmin ||Ax-b|| )
-
-Entrada: Uma matriz densa nxn A e um vetor b
-Saída: Um vetor x* nx1
-
-=#
-function minimos_quadrados(A, b)
-        return resolve_sistema(A'*A, A'*b) # Resolvo o sistema A’Ax=A’b
-end
-
-#=
-Resolve um sistema Ax = b onde A eh uma matriz nxn e b uma matriz nx1
-
-Dado um sistema Ax=b podemos decompor A em LU resolvendo o sistema:
-L(Ux) = b
-
-Onde podemos substituir (Ux) por Y e resolver o sistema
-
-LY = b
-
-E depois desfazer a substituição e resolver
-Ux = Y
-
-A: Matrix no formato (n,n)
-y: Lado direito da equacao no formato (n,1)
-
-Retorno: Solucao x
-=#
-function resolve_sistema(A, b)
-    L, U = decomposicao_lu(A) # Faz a decomposição LU
-    Y = resolve_triangular_inferior(L, b) # Resolve o sistema b
-    x = resolve_triangular_superior(U, Y)
-    return x
-end
-
-#=
-Resolve um sistema Ax = y onde A eh uma matriz triangular superior
-A: Matrix triangular superior no formato (n,n)
-y: Lado direito da equacao no formato (n,1)
-
-Retorno: Solucao x
-=#
-function resolve_triangular_superior(A, y)
-    # Podemos pegar o tamanho de y, pois eh o mesmo que A
-    n = length(y)   
-    x = zeros(n)
-    
-    # Na triangular superior, comecamos de baixo para cima, substituindo as variaveis anteriores nas equacoes
-    for i = n:(-1):1
-        x[i] = (y[i] - sum([A[i, k] * x[k] for k = i+1:n])) / A[i,i] 
-    end
-    
-    return x
-end
-
-#=
-Resolve um sistema Ax = y onde A eh uma matriz triangular inferior
-A: Matrix triangular inferior no formato (n,n)
-y: Lado direito da equacao no formato (n,1)
-
-Retorno: Solucao x
-=#
-function resolve_triangular_inferior(A, y)
-    # Podemos pegar o tamanho de y, pois eh o mesmo que A
-    n = length(y)
-    x = zeros(n)
-    
-    # Na triangular inferior, comecamos de cima para baixo, substituindo as variaveis anteriores nas equacoes
-    for i = 1:n
-        x[i] = (y[i] - sum([A[i, k] * x[k] for k = 1:i-1])) / A[i,i] 
-    end
-    
-    return x
-end
-
-#=
-Resolve um sistema Ax = y onde A eh uma matriz diagonal
-A: Matrix diagonal no formato (n,n)
-y: Lado direito da equacao no formato (n,1)
-
-Retorno: Solucao x
-=#
-function resolve_diagonal(A, y)
-    # Podemos pegar o tamanho de y, pois eh o mesmo que A
-    n = length(y)
-    
-    # Divide o lado direito pelo coeficiente de cada variavel
-    x = [y[i]/A[i,i] for i = 1:n]
-    
-    return x
-end
-
-#=
-Realiza a decomposicao LU de uma matriz quadrada A
-A: Matrix no formato (n,n)
-
-Retorno: Matrizes L & U da decomposicao LU
-=#
-function decomposicao_lu(A)
-    # Podemos salvar apenas um tamanho pois sao o mesmo
-    n, = size(A)
-    
-    # U comeca como uma copia de A, enquanto L comeca como uma matriz identidade
-    U = copy(A)
-    L = Matrix(1.0I, n, n)
-    
-    for i = 1:n
-        for j = i+1:n
-            # Eh calculado o coeficiente dividindo o numero da matriz pelo pivot
-            l = U[j,i] / U[i,i]
-            # O coeficiente eh o elemento de L
-            L[j,i] = l
-            # E o coeficiente eh usado para alterar a linha de U
-            U[j,:] += -l * U[i,:]
-        end
-    end
-    return L, U
-end
-
-#=
-Calcula a matriz inversa usando decomposicao LU
-A: Matriz no formato (n,n) para encontrar a inversa
-
-Retorno: Inversa de A
-=#
-function inverse_LU(A)
-    # Decompoe em LU
-    L, U = decomposicao_lu(A)
-    n, = size(A)
-    
-    # Inicializa a inversa
-    inv_A = zeros(n,n)
-    
-    # Para cada coluna
-    for i = 1:n
-        # Cria um vetor one-hot (Identidade final)
-        y = zeros(n)
-        y[i] = 1
-        
-        # Resolve o sistema para a coluna i
-        Y = resolve_triangular_inferior(L, y)
-        x = resolve_triangular_superior(U, Y)
-        
-        # Substitui a coluna i da inversa
-        inv_A[:,i] = x
-    end
-    
-    return inv_A
-end
-
-#=
-Realiza a decomposicao LU de uma matriz quadrada A e retorna as matrizes L e U
-
-Problema: Decomposicao LU
-
-Parâmetro A: Matrix no formato (n,n)
-
-Retorno: Matrizes L (triangular inferior) e U (triangular superior) da decomposicao LU
-
-Especificacao: A = LU
-=#
-function lu_decomposition(A)
-    # Podemos salvar apenas um tamanho pois sao o mesmo
-    n, = size(A)
-    
-    # U comeca como uma copia de A, enquanto L comeca como uma matriz identidade
-    U = copy(A)
-    L = Matrix(1.0I, n, n)
-    
-    for i = 1:n
-        for j = i+1:n
-            # Eh calculado o coeficiente dividindo o numero da matriz pelo pivot
-            l = U[j,i] / U[i,i]
-            # O coeficiente eh o elemento de L
-            L[j,i] = l
-            # E o coeficiente eh usado para alterar a linha de U
-            U[j,:] -= l * U[i,:]
-        end
-    end
-    return L, U
-end
-
 #============================== Funções dos problemas de 2 a 8 ===========================#
 
 ## 2. Problema: Achar um valor aproximado de uma função com informações de derivadas
@@ -589,6 +383,240 @@ end
 
 
 #============================== Fim Funções de 2 a 8 ===========================#
+
+
+## 9. Calcular a norma de um vetor v
+
+@doc raw"""
+Objetivo
+----------
+Calcular a norma de um vetor.
+
+Parâmetros
+----------
+    v : Vector{Float64}
+        Recebe um vetor
+
+Retorno
+----------
+    z : Float64
+        Retorna a norma do vetor v
+
+"""
+function vector_norm(v::Vector{Float64})
+    v = v.^2 # Elevo os elementos ao quadrado
+    soma = sum(v) # Faço o somatório dos elementos ao quadrado
+    z = sqrt(soma) # Faço a raiz quadrada dessa soma    
+    return z
+end 
+ 
+
+## 10. Problema: Resolver um sistema linear denso aproximadamente
+
+@doc raw"""
+Objetivo
+----------
+Aproximar a solução do sistema Ax=b utilizando o método de mínimos quadrados.
+
+Dado uma matriz densa mxn (m>n) A e um vetor b resolve o sistema A’Ax=A’b onde Ax* aproximadamente b ( x*=argmin ||Ax-b|| )
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma matriz densa
+
+    b : Vector{Float64}
+        Um vetor b tal que Ax=b
+
+Retorno
+----------
+    x : Vector{Float64}
+        Retorna uma aproximação da solução do sistema A’Ax=A’b
+
+"""
+function least_squares(A::Matrix{Float64}, b::Vector{Float64})
+        return solve_system(A'*A, A'*b) # Resolvo o sistema A’Ax=A’b
+end
+
+
+## 11. Problema: Resolver exatamente uma sistema linear denso
+
+@doc raw"""
+Objetivo
+----------
+Resolver um sistema Ax = b onde A eh uma matriz nxn e b uma matriz nx1 utilizando decomposição LU.
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma matriz nxn
+
+    b : Vector{Float64}
+        Um vetor tal que Ax=b
+
+Retorno
+----------
+    x : Vector{Float64}
+        Retorna a solução do sistema Ax=b
+
+"""
+function solve_system(A::Matrix{Float64}, b::Vector{Float64})
+    n,m = size(A)
+    @assert(n == m, string("Matrix must be a square. Received a (",n,",",m,")"))
+    L, U = lu_decomposition(A) # Faz a decomposição LU
+    Y = lower_triangular_solve(L, b) # Resolve o sistema LY=b
+    x = upper_triangular_solve(U, Y) # Resolve o sistema Ux=Y
+    return x
+end
+
+
+## 12. Problema: Resolver um sistema triangular superior
+
+@doc raw"""
+Objetivo
+----------
+Resolver um sistema Ax = y onde A eh uma matriz triangular superior e y uma matriz nx1
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma matriz triangular superior no formato (n,n)
+
+    y : Vector{Float64}
+        Um vetor tal que Ax=y
+
+Retorno
+----------
+    x : Vector{Float64}
+        Retorna a solução do sistema Ax=y
+
+"""
+function upper_triangular_solve(A::Matrix{Float64}, y::Vector{Float64})
+    # Podemos pegar o tamanho de y, pois eh o mesmo que A
+    n = length(y)   
+    x = zeros(n)
+    
+    # Na triangular superior, comecamos de baixo para cima, substituindo as variaveis anteriores nas equacoes
+    for i = n:(-1):1
+        x[i] = (y[i] - sum([A[i, k] * x[k] for k = i+1:n])) / A[i,i] 
+    end
+    
+    return x
+end
+
+
+## 13. Problema: Resolver um sistema triangular inferior
+
+@doc raw"""
+Objetivo
+----------
+Resolver um sistema Ax = y onde A eh uma matriz triangular inferior e y uma matriz nx1
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma Matrix triangular inferior no formato (n,n)
+
+    y : Vector{Float64}
+        Um vetor tal que Ax=y
+
+Retorno
+----------
+    x : Vector{Float64}
+        Retorna a solução do sistema Ax=y
+
+"""
+function lower_triangular_solve(A::Matrix{Float64}, y::Vector{Float64})
+    # Podemos pegar o tamanho de y, pois eh o mesmo que A
+    n = length(y)
+    x = zeros(n)
+    
+    # Na triangular inferior, comecamos de cima para baixo, substituindo as variaveis anteriores nas equacoes
+    for i = 1:n
+        x[i] = (y[i] - sum([A[i, k] * x[k] for k = 1:i-1])) / A[i,i] 
+    end
+    
+    return x
+end
+
+
+## 14. Problema: Resolver um sistema diagonal
+
+@doc raw"""
+Objetivo
+----------
+Resolver um sistema Ax = y onde A eh uma matriz triangular inferior e y uma matriz nx1
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma Matrix triangular inferior no formato (n,n)
+
+    y : Vector{Float64}
+        Um vetor tal que Ax=y
+
+Retorno
+----------
+    x : Vector{Float64}
+        Retorna a solução do sistema Ax=y
+
+"""
+function solve_diagonal(A::Matrix{Float64}, y::Vector{Float64})
+    # Podemos pegar o tamanho de y, pois eh o mesmo que A
+    n = length(y)
+    
+    # Divide o lado direito pelo coeficiente de cada variavel
+    x = [y[i]/A[i,i] for i = 1:n]
+    
+    return x
+end
+
+
+## 15. Problema: achar a inversa de uma matriz
+
+@doc raw"""
+Objetivo
+----------
+Achar a inversa de uma matriz A utilizando o método da decomposição LU.
+
+Parâmetros
+----------
+    A : Matrix{Float64}
+        Recebe uma Matriz no formato (n,n)
+
+Retorno
+----------
+    inv_A : Matrix{Float64}
+        Retorna a inversa da matriz A
+
+"""
+function inverse_LU(A::Matrix{Float64})
+    # Decompoe em LU
+    L, U = decomposicao_lu(A)
+    n, = size(A)
+    
+    # Inicializa a inversa
+    inv_A = zeros(n,n)
+    
+    # Para cada coluna
+    for i = 1:n
+        # Cria um vetor one-hot (Identidade final)
+        y = zeros(n)
+        y[i] = 1
+        
+        # Resolve o sistema para a coluna i
+        Y = resolve_triangular_inferior(L, y)
+        x = resolve_triangular_superior(U, Y)
+        
+        # Substitui a coluna i da inversa
+        inv_A[:,i] = x
+    end
+    
+    return inv_A
+end
+
+
+
 
 #=
 
